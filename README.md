307bd7dbace61c793aec7cd327366f53eb28605c
ajay.baljoshi <ajay.baljoshi@gmail.com>
2020-10-26 12:53:45 +0530

    updated files for ci/cd

--- README.md
+++
@@ -1,2 +0,0 @@
-# Docker
-My Docker files
--- auto_complete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Script to install auto completion for docker
-yum -y install bash-completion
-curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
-
-echo "To make change log out and login again"
